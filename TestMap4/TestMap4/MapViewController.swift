1a15c687f5b0c30837140ffb6e3056e7b5346be6
Myles Cashwell <myles@Julies-MacBook-Pro-2.local>
2021-05-27 09:26:30 -0700

    Set Map Region

--- TestMap4/TestMap4/MapViewController.swift
+++ TestMap4/TestMap4/MapViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class MapViewController: UIViewController {
     
@@ -16,21 +17,69 @@ class MapViewController: UIViewController {
     
     
     // MARK: - Properties
-    
+    let locationManager = CLLocationManager()
+    let regionInMeters: Double = 10000
     
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        checkLocationServices()
+        centerViewOnUserLocation()
     }
     
     
     // MARK: - Functions
+    func setUpLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
     
+    func checkLocationServices() {
+        if CLLocationManager.locationServicesEnabled() {
+            setUpLocationManager()
+        } else {
+            // MC -
+        }
+    }
     
-    
+    func centerViewOnUserLocation() {
+        if let location = locationManager.location?.coordinate {
+            let region = MKCoordinateRegion.init(center: location, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
+            mapView.setRegion(region, animated: true)
+        }
+    }
 }
 
 // MARK: - Extensions
-
+extension MapViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let region = MKCoordinateRegion.init(center: center, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
+        mapView.setRegion(region, animated: true)
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        switch manager.authorizationStatus {
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted:
+            // MC -
+            break
+        case .denied:
+            // MC -
+            break
+        case .authorizedAlways:
+            // MC -
+            break
+        case .authorizedWhenInUse:
+            mapView.showsUserLocation = true
+            centerViewOnUserLocation()
+            locationManager.startUpdatingLocation()
+            break
+        @unknown default:
+            print("error")
+        }
+    }
+}
