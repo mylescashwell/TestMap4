5393ab38b3c1d802158c67ea15d4587f52d798b6
Myles Cashwell <myles@Julies-MacBook-Pro-2.local>
2021-05-27 10:08:25 -0700

    Added pods

---
+++ TestMap4/TestMap4/GooglePlacesManager.swift
@@ -0,0 +1,12 @@
+//
+//  GooglePlacesManager.swift
+//  TestMap4
+//
+//  Created by Myles Cashwell on 5/27/21.
+//
+
+import Foundation
+
+class GooglePlacesManager {
+    static let shared = GooglePlacesManager()
+}
